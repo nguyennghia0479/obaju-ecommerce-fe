1eaba98ce43f27eac4d596eb28f7c4cebdf21d3c
nguyennghia0479 <48405680+nguyennghia0479@users.noreply.github.com>
2022-11-10 14:27:46 +0700

    fix jquery load

--- js/custom/layout.js
+++ js/custom/layout.js
@@ -2,26 +2,13 @@ function changeActiveLink() {
     var currentLocation = window.location.href;
     currentLocation = currentLocation.replace('//', '/').split('/');
     var page = currentLocation[currentLocation.length - 1]; 
-
-    // if(page.includes('groupwork-details.html') ) {
-    //     page = 'groupwork.html'
-    // } else if(page.includes('user-details.html') ) {
-    //     page = 'user-table.html'
-    // }
-
     $('.sidebar-menu a[href*="' + page + '"]').addClass('active');
 }
 
 $(function () {
-    $("#header").load('/header.html')
-    $("#footer-section").load('/footer.html')
-    $("#modal-delete").load('/modal-delete.html')
-    $("#admin-sidebar").load('/admin-sidebar.html', changeActiveLink)
-    $("#admin-footer").load('/admin-footer.html')
+    $("#header").load('header.html')
+    $("#footer-section").load('footer.html')
+    $("#modal-delete").load('modal-delete.html')
+    $("#admin-sidebar").load('admin-sidebar.html', changeActiveLink)
+    $("#admin-footer").load('admin-footer.html')
 })
-
-// $('.br-sideleft .br-sideleft-menu li a').each(function() {
-//     if ($(this).attr('href') == pathname) {
-//         $(this).addClass('active');
-//     }
-// });
